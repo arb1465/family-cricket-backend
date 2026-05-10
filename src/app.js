ecb77a319b306be173741c9dfacbad3f2cededd0
arb1465 <aasutoshbaraiya146@gmail.com>
2026-05-10 19:56:41 +0530

    update

--- src/app.js
+++ src/app.js
@@ -9,16 +9,7 @@ import paymentRoutes from "./routes/paymentRoutes.js";
 const app = express();
 
 // middleware
-app.use(cors({
-  origin: [
-      "http://localhost:5173",
-
-      "https://family-cricket-6ryeu2f3-aasutosh-baraiyas-projects.vercel.app",
-
-      "https://family-cricket.vercel.app",
-    ],
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/matches", matchRoutes);
